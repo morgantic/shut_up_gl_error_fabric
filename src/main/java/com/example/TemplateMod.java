02309308b254bc86c95d0fb1e0610a3c5482be18
morgantic <126607370+morgantic@users.noreply.github.com>
2026-08-08 12:27:23 +0200

    Initial commit

---
+++ src/main/java/com/example/TemplateMod.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.resources.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TemplateMod implements ModInitializer {
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("template");
+    public static final String VERSION = /*$ mod_version*/ "0.1.0";
+    public static final String MINECRAFT = /*$ minecraft*/ "26.1.2";
+
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        //? if !release
+        LOGGER.info("Hello Fabric world!");
+
+        LOGGER.warn("I'm still a template!");
+        LOGGER.warn("I'm still a template!");
+    }
+
+    /**
+     * Adapts to the {@link Identifier} changes introduced in 1.21.
+     */
+    public static Identifier id(String namespace, String path) {
+        //? if <1.21 {
+        /*return new Identifier(namespace, path);
+        *///?} else
+        return Identifier.fromNamespaceAndPath(namespace, path);
+    }
+}
